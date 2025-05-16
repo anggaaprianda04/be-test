aba95d882cc36f295513327d8e9444430711f5f0
anggaaprianda04 <anggaaprianda04@gmail.com>
2025-05-16 10:30:44 +0700

    refactor

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import router from './routes/api';
 import { PORT } from './utils/env';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.json());
